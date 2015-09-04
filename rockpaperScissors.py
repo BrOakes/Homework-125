6188d8245c82426dd126e6e7a7d7edcace0fbda7
BrOakes <BrOakesGames@gmail.com>
2015-09-04 16:11:52 -0400

    ad

---
+++ rockpaperScissors.py
@@ -0,0 +1,14 @@
+import random
+playing = True
+while playing:
+	playerInput = raw_input("Choose your weapon(Rock, Paper, Scissors)! ")
+	if playerInput.lower() != "scissors" and playerInput.lower() != "rock" and playerInput.lower() != "paper":
+		print("Thats not a valid choice")
+	else:
+		computerChoice = random.randrange(3)
+		if(computerChoice == 0 and playerInput.lower() == "paper" or computerChoice == 1 and playerInput.lower() == "scissors" or computerChoice == 2 and playerInput.lower() == "rock"):
+			print("You Win!")
+		elif(computerChoice == 0 and playerInput.lower() == "rock" or computerChoice == 1 and playerInput.lower() == "paper" or computerChoice == 2 and playerInput.lower() == "scissors"):
+			print ("Draw!")
+		else:
+			print("You Lose!")
