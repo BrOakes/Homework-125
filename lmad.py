3c37dc9f2092e4aa62f7d556c94e6d4ab9377524
BrOakes <BrOakesGames@gmail.com>
2015-09-13 12:47:48 -0400

    ad

--- lmad.py
+++ lmad.py
@@ -1,12 +1,33 @@
 import random
-choices = { 1: 'Bust', 2: 'Bust', 3: 'Bust'}
-choices[random.randrange(1,4)] = "Car"
-playerChoice = raw_input("Please choose a curtain(1,2 or 3) ")
-if(playerChoice != 1 or playerChoice != 2 or playerChoice != 3):
-    print("That is not a valid option")
-else:
-    ####Start here
+winLoss = []
+for num in range(0,50):
+	choices = { 1: 'Bust', 2: 'Bust', 3: 'Bust'}
+	choices[random.randrange(1,4)] = "Car"
+	# playerChoice = raw_input("Please choose a curtain(1,2 or 3) ")
+	playerChoice = random.randrange(1,4)
+	optionLeft = 0
+	if(int(playerChoice) != 1 and int(playerChoice) != 2 and int(playerChoice) != 3):
+	    print("That is not a valid option")
+	else:
+		prize = choices[int(playerChoice)]
+		del choices[int(playerChoice)]
+		for choice in choices:
+			if choices[choice] == "Bust":
+				del choices[choice]
+				break
+		for choice in choices:
+			optionLeft = choice
+	# switch = raw_input("We have removed one of the options, the only option left is " + str(optionLeft) + ". Would you like to swap? (y or n) ")
+	switch = 'y'
+	if(switch == 'y'):
+		playerChoice = optionLeft
+		prize = choices[int(playerChoice)]
 
-for choice in choices:
-    print (choices[choice])
-    
+	# print(choices[playerChoice])
+	if (prize == "Car"):
+		winLoss.append("W")
+	else:
+		winLoss.append("L")
+
+print(winLoss)
+	#print("Behind the curtain you chose is a " + prize)
